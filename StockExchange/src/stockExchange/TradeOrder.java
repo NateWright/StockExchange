e04d3fe7cc34616079784ec3e066b31ed0ec14fe
Nazarii Klymok <56260037+Nazarii05@users.noreply.github.com>
2019-10-08 08:43:40 -0400

    TradeOrder added
    Data carrier for fields that correspond to the entry fields in a TraderWindow

---
+++ StockExchange/src/stockExchange/TradeOrder.java
@@ -0,0 +1,60 @@
+package stockExchange;
+
+/*
+ * Data carrier for fields that correspond to the entry fields in a TraderWindow
+ * Created by: Nazarii Klymok
+ */
+public class TradeOrder {
+	private String stockSymbol;
+	private int numberOfShares;
+	private int price;
+	private boolean isBuying;
+	private boolean isMarket;
+	
+	public TradeOrder() {
+		
+	}
+
+	public boolean isMarket() {
+		return isMarket;
+	}
+
+	public void setMarket(boolean isMarket) {
+		this.isMarket = isMarket;
+	}
+
+	public boolean isBuying() {
+		return isBuying;
+	}
+
+	public void setBuying(boolean isBuying) {
+		this.isBuying = isBuying;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getNumberOfShares() {
+		return numberOfShares;
+	}
+
+	public void setNumberOfShares(int numberOfShares) {
+		this.numberOfShares = numberOfShares;
+	}
+
+	public String getStockSymbol() {
+		return stockSymbol;
+	}
+
+	public void setStockSymbol(String stockSymbol) {
+		this.stockSymbol = stockSymbol;
+	}
+	
+
+
+}
