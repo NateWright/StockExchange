449b93bdaa865fb57638e37e2c9b7c7c8dd56909
tbances <56259956+tbances@users.noreply.github.com>
2019-10-10 08:48:31 -0400

    Updated Trader
    Wow

---
+++ StockExchange/src/stockExchange/Trader.java
@@ -0,0 +1,20 @@
+package stockExchange;
+
+
+public class Trader {
+	private String name;
+public String getName() {
+	return name;
+}
+
+public void quit() {
+
+	
+}
+public void getQuote(String quote) {
+	
+}
+public void placeOrder(String order) {
+	
+}
+}
