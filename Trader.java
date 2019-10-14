9020eb8939f9db4490a9d6f54f4f892d6d51d1ff
tbances <56259956+tbances@users.noreply.github.com>
2019-10-14 16:52:25 -0400

    Add files via upload
    Updated Trader to be able to work with TradeOrder

---
+++ Trader.java
@@ -0,0 +1,23 @@
+package stockExchange;
+
+
+public class Trader {
+	private String name;
+public Trader() {
+	
+}
+public String getName() {
+	return name;
+}
+
+public void quit() {
+
+	
+}
+public void getQuote(String quote) {
+	
+}
+public void placeOrder(TradeOrder tradeOrder) {
+	
+}
+}
