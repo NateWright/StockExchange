a189c97cfe5cd08097aa2d40bc608d92314ffd72
tbances <56259956+tbances@users.noreply.github.com>
2019-10-15 09:07:03 -0400

    Updated Trader
    WOWIE BOIOIOIOPI

--- StockExchange/src/stockExchange/Trader.java
+++ StockExchange/src/stockExchange/Trader.java
@@ -1,11 +1,19 @@
 package stockExchange;
 
+import java.util.LinkedList;
+
 
 public class Trader {
 	private String name;
+	private LinkedList<String> mailbox;
+	private showMessage msg;
+	
+//No args constructor to allow for blank slates	
 public Trader() {
 	
 }
+
+//These methods allow TraderWindow to access specific information of the user 
 public String getName() {
 	return name;
 }
@@ -14,10 +22,20 @@ public void quit() {
 
 	
 }
+
 public void getQuote(String quote) {
 	
 }
+
 public void placeOrder(TradeOrder tradeOrder) {
 	
+}
+//These methods allow TraderWindow to access specific information of the user  ^^^
+
+public void mail(String message) {
+	mailbox.add(message);
+	while (!mailbox.isEmpty()) {
+	showMessage(mailbox.get(1));
+	}
 }
 }
