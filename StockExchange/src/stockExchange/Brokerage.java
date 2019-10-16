a3c31e334fff08e626660f8369c29f05895c4cb8
naustinmk12 <56259660+naustinmk12@users.noreply.github.com>
2019-10-16 08:14:27 -0400

    Update Brokerage.java

--- StockExchange/src/stockExchange/Brokerage.java
+++ StockExchange/src/stockExchange/Brokerage.java
@@ -8,15 +8,19 @@ public class Brokerage {
 		private TreeMap<String, Trader> registered;
 		private TreeSet<Trader> loggedIn;
 	
-	public Brokerage(StockExchange test)
+	//Constructor
+	public Brokerage()
 	{
 		
 	}
+	
+		//Registers a user
 		public void addUser(String userName, Trader trader)
 		{
 		 registered.put(userName, trader);
 		}
-	
+		
+		//Logs in a user
 		public void login(String userName, Trader trader)
 		{
 		 loggedIn.add(registered.get(userName));
