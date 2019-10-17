abd633c91a6d3b366b62a94ec35c53d56363f0c0
Nazarii Klymok <56260037+Nazarii05@users.noreply.github.com>
2019-10-17 08:29:34 -0400

    Updated 4 files

---
+++ Brokerage.java
@@ -0,0 +1,51 @@
+package stockExchange;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Brokerage implements Login {
+
+		private TreeMap<String, Trader> registered;
+		private TreeSet<Trader> loggedIn;
+		private StockExchange se;
+	
+	public Brokerage(StockExchange test)
+	{
+		
+		registered = new TreeMap<>();
+		loggedIn = new TreeSet<>();
+		se = test;
+	}
+		
+	public int addUser(String userName, String password)
+	{
+		if (userName.length() < 4 || userName.length() > 10)
+			return -1;
+		if (password.length() < 2 || password.length() > 10)
+			return -2;
+		if (registered.containsKey(userName))
+			return -3;
+		
+		registered.put(userName, new Trader(userName, password, this));
+		return 0;
+	}
+	
+	public int login(String userName, String password)
+	{
+		if (!userName.contains(userName))
+			return -1;
+		if (!registered.get(userName).getPassword().equals(password))
+			return -2;
+		if (loggedIn.contains(registered.get(userName)))
+			return -3;
+		
+		loggedIn.add(registered.get(userName));
+		registered.get(userName).showWindow();
+		return 0;
+	}
+	
+	public String getQuote(String smbl) {
+		return se.getStock(smbl).toString();
+	}
+	
+}
---
+++ Stock.java
@@ -0,0 +1,60 @@
+package stockExchange;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/*
+ * Stores a stock's details
+ * Created by Jared Crawford
+ */
+public class Stock {
+	private String tiker;
+	private String companyName;
+	private double price;
+	private double lowestSell;
+	private double HighestSell;
+	private int volume;
+	private PriceComparator buyComparator = new PriceComparator(false);
+	private PriceComparator sellComparator = new PriceComparator(true);
+	private PriorityQueue<TradeOrder> buyStockQ = new PriorityQueue<>(buyComparator);
+	private PriorityQueue<TradeOrder> sellStockQ = new PriorityQueue<>(sellComparator);
+	
+	
+	//The Stock object for storing the details of a stock
+	public Stock(String tik, String cName, double p) {
+		tiker = tik;
+		companyName = cName;
+		price = p;
+	}
+	
+	@Override
+	public String toString() {
+		return tiker + " " + companyName + " " + price;
+	}
+	
+	public void buyStock(TradeOrder to) {
+		buyStockQ.add(to);
+	}
+	public void sellStock(TradeOrder to) {
+		sellStockQ.add(to);
+	}
+	public double getLowestSell() {
+		return lowestSell;
+	}
+	public void setLowestSell(double lowestSell) {
+		this.lowestSell = lowestSell;
+	}
+	public double getHighestSell() {
+		return HighestSell;
+	}
+	public void setHighestSell(double highestSell) {
+		HighestSell = highestSell;
+	}
+	public int getVolume() {
+		return volume;
+	}
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
+}
